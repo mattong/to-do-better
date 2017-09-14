b37e0deb36ff3789c6a6c438bd71e5440d3c6992
mattong <matt6ong6@gmail.com>
2017-09-14 17:30:07 +0800

    Changes array to Object

--- src/index.js
+++ src/index.js
@@ -4,47 +4,48 @@ import ReactDOM from 'react-dom';
 class TodoApp extends React.Component{
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
-    this.state = {items: [], text: ''};
+    this.handleEdit = this.handleEdit.bind(this);
+    this.state = {
+      items: {
+          [`${+Date.now()}`]: { content_id: +Date.now(), text: 'luh' }
+      }
+    }
   }
+
 
   render(){
     return(
       <div>
         <h3>To Do mo 'to!</h3>
-        <TodoList items = {this.state.items} onDelete = {this.handleDelete}/>
-        <form onSubmit = {this.handleSubmit}>
-          <input onChange = {this.handleChange} value = {this.state.text} required = {true} />
-          <button> {'Add #' + (this.state.items.length +1)} </button>
-        </form>
+        <TodoList
+          items = {this.state.items}
+          onDelete={this.handleDelete}
+          onEdit={this.handleEdit}
+          onSubmit={this.handleSubmit}
+        />
+        <InputForm value={this.state.text} onSubmit={this.handleSubmit} items={this.state.items}/>
       </div>
     );
   }
 
-  handleChange(e) {
-    this.setState({text: e.target.value});
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
+  handleSubmit(value) {
     var newItem = {
-      text: this.state.text,
-      id: Date.now()
+      [`${+Date.now()}`]:
+      {
+        text: value,
+        content_id: +Date.now()
+      }
     };
-    this.setState((prevState) => ({
-      items: prevState.items.concat(newItem),
-      text: ''
-    }));
+    this.setState({
+      items: {...this.state.items, ...newItem}
+    });
   }
 
   handleDelete(itemId) {
-    let newItems = this.state.items;
-    let itemIndex = newItems.map(function(item) {return item.id}).indexOf(itemId)
-    newItems.splice(itemIndex, 1)
     this.setState({
-      items: newItems
+      items: {...this.state.items, [itemId]: undefined}
     })
   }
 
@@ -53,18 +54,42 @@ class TodoApp extends React.Component{
   }
 }
 
-const TodoList = ({items, onDelete}) => {
+const TodoList = ( {items, onDelete, onSubmit, value} ) => {
+  console.error(items)
   return(
     <ul>
-      {items.map(item =>(
-      <li key={item.id}> {item.text} <EditListItem/><button onClick = {e => {e.preventDefault(); onDelete(item.id)}}> Delete </button></li>
-      ))}
+      {Object.keys(items).filter(id => items[id] !== undefined).map(id => {
+      return <li key={ items[id].content_id }>
+          { items[id].text }
+          <EditListItem items={items} value={value} onSubmit={onEdit} />
+          <button onClick={e => onDelete(items[id])}> Delete </button>
+        </li>
+      })}
     </ul>
   )
 }
 
-const EditListItem = () => {
-  return <button>Edit</button>
+const EditListItem = ( { value, onEdit, items} ) => {
+  return(
+  <div>
+    <button >Edit</button>
+    <InputForm  value={value} onSubmit={onSubmit} items={items} />
+  </div>
+  )
+}
+
+const InputForm = ( {value, onSubmit, items} ) => {
+  let me;
+  return(
+    <form onSubmit = {e =>{
+        e.preventDefault()
+        onSubmit(me.value)
+        me.value = ''
+    }}>
+      <input ref={el => me = el}   value={value} required autoFocus />
+      <button> {'Lagay mo na to sa #' + (Object.entries(items).length +1)} </button>
+    </form>
+  )
 }
 
 
